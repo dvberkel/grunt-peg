74ece535a979462afcb22dfd19eb825a2906e21e
Will Elwood <w.elwood08@gmail.com>
2013-07-03 06:45:16 +0100

    Implement support for standard Grunt-0.4 file mappings and options.
    See https://github.com/gruntjs/grunt/wiki/Configuring-tasks#files and https://github.com/gruntjs/grunt/wiki/Configuring-tasks#options for details.
    Retains backwards compatibility with existing grammar/outputFile/exportVar configuration format.

--- tasks/peg.js
+++ tasks/peg.js
@@ -9,24 +9,55 @@
 'use strict';
 
 var PEG = require('pegjs');
-var extend = require('extend');
-
-var parserSource = function(grammar, exportVar, options){
-  exportVar = exportVar || 'module.exports';
-  options = extend({}, { cache: false, trackLineAndColumn: false }, options);
-  var parser = PEG.buildParser(grammar, options);
-
-  return exportVar + ' = ' + parser.toSource() + ';';
-};
 
 module.exports = function(grunt) {
-
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
   grunt.registerMultiTask('peg', 'Generates parsers from PEG grammars.', function() {
-    grunt.log.write(grunt.template.process('Generating parser from <%= grammar %>', this));
-    var grammar = grunt.file.read(this.data.grammar);
-    grunt.file.write(this.data.outputFile, parserSource(grammar, this.data.exportVar, this.data.options));
+    // Standard Grunt "src(s) -> dest" support.
+    var files = this.files;
+
+    // Legacy "grammar -> outputFile" support.
+    if(this.data.grammar && this.data.outputFile) {
+      files.push({
+        src: [this.data.grammar],
+        dest: this.data.outputFile
+      });
+    }
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      exportVar: this.data.exportVar /* legacy option */ || 'module.exports',
+      cache: false,
+      trackLineAndColumn: false
+    });
+
+    // Iterate over all src-dest file pairs.
+    files.forEach(function(f) {
+      // Warn on and remove invalid source files (if nonull was set).
+      var src = f.src.filter(function(filepath) {
+        if(!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      });
+
+      grunt.log.writeln('Generating parser from "' + src.join('", "') + '"...');
+
+      // Read in all source files and concatenate with newlines.
+      var grammar = src.map(function(filepath) {
+        return grunt.file.read(filepath);
+      }).join(grunt.util.linefeed);
+
+      // Generate the parser.
+      var time = Date.now();
+      var parser = PEG.buildParser(grammar, options);
+      time = Date.now() - time;
+
+      // Save the parser.
+      grunt.file.write(f.dest, options.exportVar + ' = ' + parser.toSource() + ';');
+
+      grunt.log.writeln('Parser "' + f.dest + '" generated in ' + time + 'ms.');
+    });
   });
 };
