4facfeae4e4d87b5ca28deea56032b5b71a01822
Felix Hao <felixhao28@gmail.com>
2015-09-10 18:07:31 +0800

    remove unused npm task

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,7 +100,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-bumpup');
-  grunt.loadNpmTasks('grunt-exec');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
